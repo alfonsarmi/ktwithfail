49cf893ed0866ac9e1a11279c7b4aa4f8d2c9fa0
Alfonso Sarmiento Ráez <asarmiento@foreworth.com>
2023-12-13 11:09:34 +0100

    First Commit

---
+++ car.kt
@@ -0,0 +1,48 @@
+package ParserComplex.CarControl
+
+class Car(val make: String, val model: String, val year: Int, val color: String) {
+    private var speed: Int = 0
+    private var engineOn: Boolean = false
+
+    fun startEngine() {
+        engineOn = true
+        println("The $color $make $model's engine is now on.")
+    }
+
+    fun stopEngine() {
+        engineOn = false
+        println("The $color $make $model's engine is now off.")
+    }
+
+    fun accelerate(amount: Int) {
+        if (engineOn) {
+            speed += amount
+            println("The $color $make $model is now traveling at $speed km/h.")
+        } else {
+            println("The $color $make $model's engine is not on.")
+        }
+    }
+
+    fun brake(amount: Int) {
+        if (engineOn) {
+            if (speed > amount) {
+                speed -= amount
+                println("The $color $make $model slowed down to $speed km/h.")
+            } else {
+                speed = 0
+                println("The $color $make $model stopped.")
+            }
+        } else {
+            println("The $color $make $model's engine is not on.")
+        }
+    }
+
+    fun getInfo() {
+        println("Make: $make")
+        println("Model: $model")
+        println("Year: $year")
+        println("Color: $color")
+        println("Speed: $speed km/h")
+        println("Engine On: $engineOn")
+    }
+}
